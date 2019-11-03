8e60ddd227666788acf7ee41516b6e19b0932d4a
kinglobal <kinglobal@qq.com>
2019-11-04 01:14:15 +0800

    wuziqi

---
+++ js/script.js
@@ -0,0 +1,62 @@
+﻿  //创建二维绘图
+  var chess = document.getElementById('chess');
+  var context = chess.getContext('2d');
+  //设置颜色
+  context.strokeStyle = "#BFBFBF";
+
+  //添加棋盘背景图片
+  var lwj = new Image();
+  lwj.src = "../images/bo.jpg"
+  lwj.onload = function() {
+  //图片从点0，0到450,450
+  context.drawImage(lwj, 0, 0, 450, 450);
+  qipan();   //调用qipan函数
+  }
+
+  //---棋盘---
+  function qipan() {
+    for (var i = 0; i < 15; i++ ) {
+      //横线---留白15px，间隔30px
+      context.moveTo(15 + i*30, 15);
+      context.lineTo(15 + i*30, 435);
+      context.stroke();
+
+      //纵线---留白15px，间隔30px
+      context.moveTo(15, 15 + i*30);
+      context.lineTo( 435, 15 + i*30);
+      //stroke() 方法会实际地绘制路径
+      context.stroke();
+    }
+  }
+
+  //---棋子---
+  function qizi(x, y, me1) {
+    context.beginPath();
+    //arc() 方法创建弧/曲线---x y 半径 起点角 终点角
+    context.arc(15 + x*30, 15 + y*30, 13, 0, 2 * Math.PI);
+    //closePath() 方法创建从当前点到开始点的路径
+    context.closePath();
+
+    //---fill的style 创建渐变色圆---
+    var gradient = context.createRadialGradient(15 + x*30 + 2, 15 + y*30 - 2, 13, 15 + x*30 + 2, 15 + y*30 - 2, 0);
+    //判断棋子颜色
+    if(me1) {
+      gradient.addColorStop(0, "#0A0A0A");
+      gradient.addColorStop(1, "#ffffff");
+    } else {
+      gradient.addColorStop(0, "#cccccc");
+      gradient.addColorStop(1, "#ffffff");
+    }
+    //fill() 方法填充当前的图像（路径）
+    context.fillStyle = gradient;
+    context.fill();
+  }
+
+  //---限制---
+  var chessLimit = [];
+  for (var i = 0; i < 15; i++) {
+    chessLimit[i] = [];
+    for (var j = 0; j < 15; j++) {
+      chessLimit[i][j] = 0;
+    }
+  }
---
+++ js/win.js
@@ -0,0 +1,105 @@
+﻿//赢法数组
+var wins = [];
+var count = 0;
+
+for (var i = 0; i < 15; i++) {
+  wins[i] = [];
+  for (var j = 0; j < 15; j++) {
+    wins[i][j] = [];
+  }
+}
+//横线所有赢法
+for (var i = 0; i < 15; i++) {
+  for (var j = 0; j < 11; j++) {
+    for (var k = 0; k < 5; k++) {
+      wins[i][j+k][count] = true;
+    }
+  count++;
+  }
+}
+//竖线所有赢法
+for (var i = 0; i < 15; i++) {
+  for (var j = 0; j < 11; j++) {
+    for (var k = 0; k < 5; k++) {
+      wins[j+k][i][count] = true;
+    }
+  count++;
+  }
+}
+//斜线所有赢法
+for (var i = 0; i < 11; i++) {
+  for (var j = 0; j < 11; j++) {
+    for (var k = 0; k < 5; k++) {
+      wins[i+k][j+k][count] = true;
+    }
+  count++;
+  }
+}
+//反斜线所有赢法
+for (var i = 0; i < 11; i++) {
+  for (var j = 14; j > 3; j--) {
+    for (var k = 0; k < 5; k++) {
+      wins[i+k][j-k][count] = true;
+    }
+  count++;
+  }
+}
+
+//打印count看看总共有几种赢法
+// console.log(count);
+
+//棋子的统计数组
+var myWin = [];
+var youWin = [];
+
+for (var i = 0; i < count; i++) {
+  myWin[i] = 0;
+  youWin[i] = 0;
+}
+
+//---实现下棋，为chess绑定onclick事件
+var me = true;
+var over = false;
+
+chess.onclick = function(e) {
+  if(over) {
+    return;
+  }
+  //棋盘坐标
+  var x = e.offsetX;
+  var y = e.offsetY;
+
+  //棋子索引算法x除以30 然后向下取整
+  var i = Math.floor(x / 30);
+  var j = Math.floor(y / 30);
+  //下棋时判断(limit)
+  if (chessLimit[i][j] == 0) {
+    qizi(i, j, me);
+  if(me) {
+    //黑棋赢法
+    chessLimit[i][j] = 1;
+    for(var k=0; k<count; k++) {
+      if(wins[i][j][k]) {
+        myWin[k]++;
+        if(myWin[k] == 5) {
+          window.alert("黑棋赢");
+          over = true;
+        }
+      }
+    }
+  } else { 
+    //白棋赢法
+    chessLimit[i][j] = 2;
+    for(var k=0; k<count; k++) {
+      if(wins[i][j][k]) {
+        youWin[k]++;
+        if(youWin[k] == 5) {
+          window.alert("白棋赢");
+          over = true;
+        }
+      }
+    }
+  }
+     me = !me;
+  }
+}
